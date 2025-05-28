e77452bef6ae2b25d7c6ededa924fc87c9084f86
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2025-05-28 20:05:21 +0200

    Mejorando cositas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`bg-black ${geistSans.variable} ${geistMono.variable}`}>
      
           <Header />
         <main>{children}</main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,70 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="bg-black min-h-screen p-5 pb-60 gap-10 flex flex-col items-center justify-center ">
-      <h1 className="text-white text-center text-6xl font-bold "> 
-        Information you need during <br />on-call emergencies
-      </h1>
-      <p className="text-white text-center text-lg">
-        Quickly link new on-call tickets to similar past incidents and their solutions. All <br />directly in Slack the moment an incident happens.
-      </p>
-      <Link href="/Docs" className="p-5">
-        <Button className="bg-white text-black hover:bg-gray-400 px-10 py-6">
-          Get Started &gt;
-        </Button>
-      </Link>
+    <div className="bg-black min-h-screen p-5 pb-60 gap-20 flex flex-col items-center justify-center mt-20 font-[family-name:var(--font-geist-sans)]">
+      
+      <section className="max-w-4xl text-center">
+        <h2 className="text-white text-6xl font-bold mb-4">
+          Information you need during <br />
+          on-call emergencies
+        </h2>
+        <p className="text-white text-lg mb-8">
+          Quickly link new on-call tickets to similar past incidents and their
+          solutions. All <br />
+          directly in Slack the moment an incident happens.
+        </p>
+       
+        <Link href="/Docs" className="p-5">
+          <Button className="bg-white text-black hover:bg-gray-400 px-10 py-6">
+            Get Started &gt;
+          </Button>
+        </Link>
+        
+         <br />
+        <br />
+        <img
+          src="/imagen.png"
+          alt=""
+          className="rounded mx-auto"
+          style={{ width: 1000, height: 600 }}
+        />
+      </section>
       <br />
-           <img src="/imagen.png" alt="" className="rounded" style={{ width: 1000, height: 600 }} />
-<br />
-<br />
-      <h1 className="text-white text-center text-5xl font-bold "> 
-        Instant setup, no custom <br />code
-      </h1>
-       <p className="text-white text-center text-lg">
-        Quickly link new on-call tickets to similar past incidents and their solutions. All <br />directly in Slack the moment an incident happens.
-      </p>
+      <br />
+      <section className="max-w-3xl text-center">
+        <h2 className="text-white text-5xl font-bold mb-4">
+          Instant setup, no custom <br />
+          code
+        </h2>
+        <p className="text-white text-lg mb-8">
+          Invite the bot, pick a channel, and you're set—no custom code needed, and no <br /> vendor lock-in.
+        </p>
+        <img
+          src="/imagen.png"
+          alt=""
+          className="rounded mx-auto"
+          style={{ width: 700, height: 400 }}
+        />
+      </section>
+      <br />
+      <br />
 
+      <section className="max-w-4xl text-center">
+        <h2 className="text-white text-6xl font-bold mb-4">
+          Get in touch
+        </h2>
+        <p className="text-white text-lg mb-8">
+          Request a demo, or hop on a call
+        </p>
+         <Link href="/Pricing" className="p-5">
+          <Button className="bg-white text-black hover:bg-gray-400 px-10 py-6">
+            Get Started &gt;
+          </Button>
+        </Link>
+      </section>
+      <br />
+      <br />
 
     </div>
-
-
   );
 }
